eeb4f5a42bcc783c02ab76c9a20ac6b67fdc797c
中江洋史 <h_nakae@nus21.com>
2017-01-05 00:28:15 +0900

    Sqlite暗号化テスト

---
+++ padtest/padtest/SplashViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SplashViewController.swift
+//  padtest
+//
+//  Created by 中江洋史 on 2016/12/11.
+//  Copyright © 2016年 中江洋史. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        navigationBarAndStatusBarHidden(hidden: true, animated: false)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ padtest/padtest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  padtest
+//
+//  Created by 中江洋史 on 2016/12/08.
+//  Copyright © 2016年 中江洋史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
